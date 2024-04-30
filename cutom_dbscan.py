9bccc2a77942a8a047ad505d6f79a758441eba75
labinochka <labensckayam@yandex.ru>
2024-04-30 14:16:12 +0300

    update

---
+++ cutom_dbscan.py
@@ -0,0 +1,78 @@
+import pygame
+import math
+import numpy as np
+
+
+def distance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    return np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+
+def dbscan(points, epsilon, min_points):
+    clusters = {}
+    cluster_id = 0
+
+    for point in points:
+        if point in clusters:
+            continue
+
+        neighbors = []
+        for other_point in points:
+            dist = distance(point, other_point)
+            if other_point != point and distance(point, other_point) <= epsilon:
+                neighbors.append(other_point)
+
+        if len(neighbors) < min_points:
+            clusters[point] = -1
+            continue
+
+        clusters[point] = cluster_id
+
+        in_cluster = False
+
+        for neighbor in neighbors:
+            if neighbor in clusters and clusters[neighbor] != -1:
+                cluster_id = clusters[neighbor]
+                clusters[point] = cluster_id
+                in_cluster = True
+                break
+            clusters[neighbor] = cluster_id
+
+        if in_cluster:
+            for neighbor in neighbors:
+                clusters[neighbor] = cluster_id
+
+        cluster_id += 1;
+
+    return clusters
+
+
+if __name__ == '__main__':
+    points = []
+    r = 10
+    min_pts, eps = 3, 3 * r
+    colors = ['green', 'yellow', 'blue', 'purple', 'pink', 'gray', 'orange', 'red']
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    screen.fill('white')
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    point = pygame.mouse.get_pos()
+                    points.append(point)
+                    pygame.draw.circle(screen, 'black', point, r)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    clusters = dbscan(points, eps, min_pts)
+                    for point in points:
+                        cluster_id = clusters[point]
+                        color = colors[cluster_id];
+                        pygame.draw.circle(screen, color, point, r)
+
+        pygame.display.flip()
+    pygame.quit()
